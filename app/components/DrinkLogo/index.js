8256b79ab036d54f43fa425e6baec4d4a7f58b4c
Devin Matte <devinmatte@gmail.com>
2018-01-20 23:41:30 -0500

    Testing more images

--- app/components/DrinkLogo/index.js
+++ app/components/DrinkLogo/index.js
@@ -15,14 +15,15 @@ import PropTypes from 'prop-types';
 const images = [
   'http://www.coca-cola.co.uk/content/dam/journey/gb/en/hidden/Products/Brand-image/Drinks-banners/Dr-Pepper-banner.jpg',
   'http://ugaiabc.com/wp/wp-content/uploads/1907455_623263227818460_3601708294401885047_n.jpg',
-  'https://i.ytimg.com/vi/sOHPMw0EXxA/maxresdefault.jpg',
+  'http://www.coca-cola.co.uk/content/dam/journey/gb/en/hidden/Products/Brand-image/Drinks-banners/Coca-Cola-Zero-Sugar-Banner.jpg',
+  'http://www.faktxeber.com/resimler/haber/manset/253151_465312.jpg',
 ];
 
 class DrinkLogo extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div style={{ width: '100%', height: '100px', overflow: 'hidden', position: 'relative', alignItems: 'center' }}>
-        <img alt={'Drink Name'} onClick={this.props.onClick} style={{ width: '100%', position: 'absolute' }} src={images[Math.floor(Math.random() * 3)]} />
+        <img alt={'Drink Name'} onClick={this.props.onClick} style={{ width: '100%', position: 'absolute' }} src={images[Math.floor(Math.random() * 4)]} />
       </div>
     );
   }
